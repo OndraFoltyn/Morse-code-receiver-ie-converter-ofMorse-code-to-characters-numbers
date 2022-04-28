67e85a88654178ae25560f24d2d3dc67dcec804d
Ondřej Foltyn <99394423+OndraFoltyn@users.noreply.github.com>
2022-04-28 18:33:31 +0200

    Update hex7seg.vhd

--- projekt4/project_4/project_hlavni.srcs/sources_1/new/hex7seg.vhd
+++ projekt4/project_4/project_hlavni.srcs/sources_1/new/hex7seg.vhd
@@ -49,73 +49,73 @@ begin
                     when "001" => -- if character output from decoder is 1
                         case shift_reg_i is
                             when "00000" => -- 
-                                seg2_o <= "0110000";-- E .
+                                seg2_o <= "0110000";-- E 
                             when others =>
-                                seg2_o <= "1111110"; -- -
+                                seg2_o <= "1111110"; 
                         end case;
                         
                     when "010" => -- 2 char
                         case shift_reg_i is
                             when "10000" => -- 
-                                seg2_o <= "0001000";-- A .-
+                                seg2_o <= "0001000";-- A 
                             when others =>
-                                seg2_o <= "1111110"; -- -
+                                seg2_o <= "1111110"; 
                         end case;
                         
                     when "011" => -- 3 char
                         case shift_reg_i is
                             when "00100" =>
-                                seg2_o <= "1000010";-- d -..
+                                seg2_o <= "1000010";-- d 
                             when "10000" =>
-                                seg2_o <= "1000001";-- U ..-
+                                seg2_o <= "1000001";-- U
                             when "00000" =>
-                                seg2_o <= "0100100";-- S ...
+                                seg2_o <= "0100100";-- S 
                             when others =>
-                                seg2_o <= "1111110"; -- -
+                                seg2_o <= "1111110"; 
                         end case;
                         
                     when "100" =>  -- 4 char
                         case shift_reg_i is
                             when "00010" =>
-                                seg2_o <= "1100000";-- b -...
+                                seg2_o <= "1100000";-- b 
                             when "01010" =>
-                                seg2_o <= "0110001";-- C -.-.
+                                seg2_o <= "0110001";-- C 
                             when "01000" =>
-                                seg2_o <= "0111000";-- F ..-.
+                                seg2_o <= "0111000";-- F 
                             when "01100" =>
-                                seg2_o <= "0011000";-- P .--.
+                                seg2_o <= "0011000";-- P 
                             when "00100" =>
-                                seg2_o <= "1110001";-- L .-..
+                                seg2_o <= "1110001";-- L 
                             when "00000" =>
-                                seg2_o <= "1001000";-- H ....
+                                seg2_o <= "1001000";-- H 
                             when others =>
-                                seg2_o <= "1111110"; -- -
+                                seg2_o <= "1111110"; 
                         end case;
                         
                     when "101" => -- 5 char
                           case shift_reg_i is
                             when "11111" =>
-                                seg2_o <= "0000001";-- 0 -----
+                                seg2_o <= "0000001";-- 0 
                             when "11110" =>
-                                seg2_o <= "1001111";-- 1 .----
+                                seg2_o <= "1001111";-- 1 
                             when "11100" =>
-                                seg2_o <= "0010010";-- 2 ..---
+                                seg2_o <= "0010010";-- 2 
                             when "11000" =>
-                                seg2_o <= "0000110";-- 3 ...--
+                                seg2_o <= "0000110";-- 3 
                             when "10000" =>
-                                seg2_o <= "1001100";-- 4 ....-
+                                seg2_o <= "1001100";-- 4 
                             when "00000" =>
-                                seg2_o <= "0100100";-- 5 .....
+                                seg2_o <= "0100100";-- 5 
                             when "00001" =>
-                                seg2_o <= "0100000";-- 6 -....
+                                seg2_o <= "0100000";-- 6 
                             when "00011" =>
-                                seg2_o <= "0001111";-- 7 --...
+                                seg2_o <= "0001111";-- 7 
                             when "00111" =>
-                                seg2_o <= "0000000";-- 8 ---..
+                                seg2_o <= "0000000";-- 8 
                             when "01111" =>
-                                seg2_o <= "0000100";-- 9 ----.
+                                seg2_o <= "0000100";-- 9 
                             when others =>
-                                seg2_o <= "1111110"; -- -
+                                seg2_o <= "1111110"; 
                         end case;
                         when others =>
                         seg2_o <= "1111110";
